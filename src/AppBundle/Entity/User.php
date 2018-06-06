a0f261eb1eeaea69fe8ca1c1b5a1e220f08ca5ac
Rafal Fidurski <rafal.fidurski@gmail.com>
2018-06-06 12:58:47 +0200

    part 1

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -9,21 +9,26 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\Table(name="users")
  */
-class User extends BaseUser
-{
+class User extends BaseUser {
+
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-    
-   
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
         // your own logic
     }
-    
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+
 }
