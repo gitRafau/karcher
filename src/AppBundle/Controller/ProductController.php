fd361a069801202bddddfa3a98b6a229a3ea4f73
Rafal Fidurski <rafal.fidurski@gmail.com>
2018-06-06 17:11:44 +0200

    Form Builder, create Product

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -2,54 +2,64 @@
 
 namespace AppBundle\Controller;
 
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use AppBundle\Entity\Product;
-
 
 /**
  * Description of ProductController
  *
  * @author Rafał
  */
-class ProductController extends Controller {   
-    
+class ProductController extends Controller {
+
     /**
-     *@Route("/cms/panel/product/create", name="product_create") 
+     * @Route("/cms/panel/product/create", name="product_create") 
      */
-    public function createProductAction(){
-        
-        $em = $this->getDoctrine()->getManager();
-        
-        $title = 'Myjka Ciśnieniowa';
-        $descript = 'Najlepsza na rynku!';
-        
-        
+    public function createProductAction(Request $request) {
+
         $product = new Product();
-        $product ->setTitle($title);
-        $product->setDescript($descript);
-        
-        $em->persist($product);
-        $em->flush();
-        
-        
-        return $this->render('cms/create.html.twig');
-    }
-    
-    /**
-     *@Route("/", name="homepage") 
-     */
-    public function allProductAction(){
-        
-        $produkty = $this->getDoctrine()->getRepository('AppBundle:Product')->findAll();
-        
-       // dump($produkty); die();
-        
-        return $this->render('karcher/index.html.twig', [
-            'produkty' => $produkty
+
+        $form = $this->createFormBuilder($product)
+                ->add('title', TextType::class)
+                ->add('descript', TextType::class)
+                ->add('serve', SubmitType::class, array('label' => 'Create Product'))
+                ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+
+            //dump($product); die();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('cms_panel');
+        }
+
+        return $this->render('cms/create.html.twig', [
+                    'form' => $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/", name="homepage") 
+     */
+    public function allProductAction() {
+
+        $produkty = $this->getDoctrine()->getRepository('AppBundle:Product')->findAll();
+
+        // dump($produkty); die();
+
+        return $this->render('karcher/index.html.twig', [
+                    'produkty' => $produkty
+        ]);
+    }
+
 }
