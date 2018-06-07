7f8570eda6080bf80d93f7f2cb4ae3e7072d5bb4
Rafal Fidurski <rafal.fidurski@gmail.com>
2018-06-07 11:45:14 +0200

    crud product

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,32 +6,36 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
+use AppBundle\Entity\Product;
+
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         // replace this example code with whatever you need
         return $this->render('karcher/index.html.twig');
     }
-    
+
     /**
      * @Route("/cms", name="cms_login")
      */
-    public function indexCMSAction(Request $request)
-    {
-        
+    public function indexCMSAction(Request $request) {
+
         return $this->render('cms/login.html.twig');
     }
-    
+
     /**
      * @Route("/cms/panel", name="cms_panel")
      */
-    public function panelCMSAction(Request $request)
-    {
-        
-        return $this->render('cms/panel.html.twig');
+    public function panelCMSAction(Request $request) {
+       
+        $produkty = $this->getDoctrine()->getRepository('AppBundle:Product')->findAll();        
+
+        return $this->render('cms/panel.html.twig', [
+                    'produkty' => $produkty
+        ]);
     }
+
 }
--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -29,10 +29,10 @@ class ProductController extends Controller {
                     'attr' => array('class' => 'form-control'),
                 ))
                 ->add('descript', TextareaType::class, array(
-                    'attr' => array('class' => 'form-control'),
+                    'attr' => array('class' => 'form-control')
                 ))
                 ->add('Dodaj Produkt', SubmitType::class, array(
-                    'attr' => array('class' => 'btn btn-info btn-block'),
+                    'attr' => array('class' => 'btn btn-info btn-block', 'style' => 'margin-top: 20px;'),
                 ))
                 ->getForm();
 
@@ -67,6 +67,69 @@ class ProductController extends Controller {
         return $this->render('karcher/index.html.twig', [
                     'produkty' => $produkty
         ]);
+    }
+
+    /**
+     * @Route("cms/panel/product/update/{id}", name="update_product")
+     */
+    public function updateProductAction(Request $request, $id) {
+
+        $product = new Product();
+
+
+        $product = $this->getDoctrine()->getRepository('AppBundle:Product')->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                    'Nie ma takie produktu numerze: ' . $id
+            );
+        }
+
+        $form = $this->createFormBuilder($product)
+                ->add('title', TextType::class, array(
+                    'attr' => array('class' => 'form-control'),
+                ))
+                ->add('descript', TextareaType::class, array(
+                    'attr' => array('class' => 'form-control')
+                ))
+                ->add('Edytuj Produkt', SubmitType::class, array(
+                    'attr' => array('class' => 'btn btn-warning btn-block', 'style' => 'margin-top: 20px;'),
+                ))
+                ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+
+            //dump($product); die();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('cms_panel');
+        }
+
+
+        return $this->render('cms/update.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/cms/panel/produkt/delete/{id}", name="delete_product") 
+     */
+    public function deleteProductAction($id, Request $request) {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $product = $em->getRepository('AppBundle:Product')->findOneBy(array('id' => $id));
+
+        $em->remove($product);
+        $em->flush();
+
+        return $this->redirectToRoute('cms_panel');
     }
 
 }
